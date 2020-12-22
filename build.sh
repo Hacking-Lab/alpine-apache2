3fc9b38894af9a8815c579989d76337b8ec19630
Ivan Buetler <ivan.buetler@compass-security.com>
2020-12-22 08:21:10 +0100

    update

--- build.sh
+++ build.sh
@@ -1,1 +1,4 @@
-docker build -t hackinglab/alpine-apache2 -f Dockerfile .
+docker build --no-cache -t hackinglab/alpine-apache2:3.2.0 -t hackinglab/alpine-apache2:3.2 -t hackinglab/alpine-apache2:latest -f Dockerfile .
+
+docker push hackinglab/alpine-apache2
+
